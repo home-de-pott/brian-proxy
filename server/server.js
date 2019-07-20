2a1b3e04334edb70f86e68130367578f642829d9
Brian Embry <b.a.embry@gmail.com>
2019-07-20 16:08:12 -0500

    Serve static assets on all routes

--- server/server.js
+++ server/server.js
@@ -5,14 +5,17 @@ const path = require('path');
 
 PORT = PORT || 3005;
 app.use(express.static(__dirname + '/../client/dist'));
+app.use('/*', express.static(__dirname + '/../client/dist'));
+// app.use('/products/', express.static(__dirname + '/../client/dist'));
+// app.use('/products/:id', express.static(__dirname + '/../client/dist'));
 
-app.get('/*', (req, res) => {
-  console.log('route hit');
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'), err => {
-    if (err) {
-      res.status(500).send('Something went wrong');
-    }
-  });
-});
+// app.get('/*', (req, res) => {
+//   console.log('route hit');
+//   res.sendFile(path.join(__dirname, '../client/dist/index.html'), err => {
+//     if (err) {
+//       res.status(500).send('Something went wrong');
+//     }
+//   });
+// });
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
